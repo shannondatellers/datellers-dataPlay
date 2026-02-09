5df8a831fa0a1d86ceda8634c3bebbc403d753a1
shannondatellers <shannon.pereira@orbussoftware.com>
2026-02-09 15:21:47 +0530

    ESLint logic updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,17 @@
 import powerbiVisualsConfigs from "eslint-plugin-powerbi-visuals";
+import tseslint from "@typescript-eslint/eslint-plugin";
 
 export default [
     powerbiVisualsConfigs.configs.recommended,
     {
         ignores: ["node_modules/**", "dist/**", ".vscode/**", ".tmp/**"],
+    },
+    {
+        plugins: {
+            "@typescript-eslint": tseslint,
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "error",
+        },
     },
 ];
